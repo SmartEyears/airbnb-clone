6339097f24d539000eed9bdf6784323dc4a368dc
SmartEyears <57298689+SmartEyears@users.noreply.github.com>
2019-12-16 20:49:53 +0900

    login_signup

--- users/models.py
+++ users/models.py
@@ -3,33 +3,37 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """Custom User Model"""
+
+    """ Custom User Model """
 
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
-    GERDER_CHOICES = (
+    GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
 
     LANGUAGE_ENGLISH = "en"
-    LANGUAGE_KOREAN = "ko"
+    LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "english"), (LANGUAGE_KOREAN, "한국어"))
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
 
     CURRENCY_USD = "usd"
-    CURRENCY_KO = "krw"
+    CURRENCY_KRW = "krw"
 
-    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KO, "KRW"))
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
-    gender = models.CharField(choices=GERDER_CHOICES, max_length=10, blank=True)
-    bio = models.TextField(default="", blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
     superhost = models.BooleanField(default=False)
-
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
-
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+    path("sigup", views.SignUpView.as_view(), name="signup"),
+]
